640959d62ffd67aabb83cd9fb92293d5a3907d43
fannysigouin <fannysigouin@Fannys-MacBook-Pro.local>
2023-11-25 11:50:56 -0500

    finalizing plots and commenting code

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,21 @@
-// Assign samples.json URL to a constant
+// assign samples.json URL to a constant
 
 const samples_url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// function for the default plot
+// function for the default plots
 function init() {
-    // select the dropdown element in index.html and store in variable
+    // select the drop-down element in index.html and store in variable
     let select = d3.select("#selDataset");
     // append names/IDs from samples to the dropdown
     d3.json(samples_url).then(function(data) {
-        // store list of names in variable
+        // store list of sample ids (names) in variable
         let sample_ids = data.names;
-        // loop through each ID in the list of names and append the ID to the select variable
+        // loop through each ID in the list and append the ID to the select variable
         sample_ids.forEach((id) => {
             select.append("option").text(id).property("value", id);
         });
 
-        // use first sample ID to build default plots
+        // use the first sample ID to build the default plots
         const firstSample = sample_ids[0];
         updatePlots(firstSample);
         updateMetadata(firstSample);
@@ -29,13 +29,13 @@ function updateMetadata(sample) {
         let metadata = data.metadata;
         // use .filter() to only return the metadata for the chosen sample by matching the ID
         let filterMetadata = metadata.filter(sampleObject => sampleObject.id == sample);
-        // assign first element of filterMetadata array (array with a dictionary) to a variable
+        // assign first element of filterMetadata array to a result variable
         let result = filterMetadata[0];
 
         // loop through each key:value pair in the metadata result dictionary and append it to the #sample-metadata html element
         // assign #sample-metadata to a variable 
         metadataPanel = d3.select("#sample-metadata").html("");
-        // use Object.entries.forEach to return key:value pairs
+        // use Object.entries.forEach to return key:value pairs and append text to metadataPanel as h5
         Object.entries(result).forEach(([key, value]) => {
             metadataPanel.append("h5").text(`${key}: ${value}`)
         });
@@ -43,21 +43,21 @@ function updateMetadata(sample) {
     });
 }
 
-// function to update the plots
+// function to update the plots based on drop-down selection
 function updatePlots(sample) {
     d3.json(samples_url).then(function(data) {
-        // assign samples to a variable
+        // assign samples dictionary to a variable
         let samples = data.samples;
-        // use .filter() to only return the sample data for the chosen sample
+        // use .filter() to only return the sample data for the chosen sample by matching the ID
         let filterSamples = samples.filter(sampleObject => sampleObject.id == sample);
-        // assign first element of filterSamples array to a variable
+        // assign first element of filterSamples array to a result variable
         let result = filterSamples[0];
         
         // assign sample_values to a variable
         let sample_values = result.sample_values;
-        // assign otu_ids to a variable to be used in the plot
+        // assign otu_ids to a variable 
         let otu_ids = result.otu_ids;
-        // assign otu_labels to a variable, then slice the first 10 elements and reverse the order
+        // assign otu_labels to a variable
         let otu_labels = result.otu_labels;
 
         // build the bar chart
@@ -66,14 +66,15 @@ function updatePlots(sample) {
             x: sample_values.slice(0, 10).reverse(),
             // slice first 10 elements of otu_ids, use .map() to format as "OTU {id}" on the plot and reverse to sort in descending order
             y: otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse(),
-            // slice first 10 elements of otu_labels and reverse to sort in descending order
+            // slice first 10 elements of otu_labels and reverse to sort in descending order for hovertext
             text: otu_labels.slice(0, 10).reverse(),
+            // set horizontal bar chart
             type: "bar",
             orientation: "h"
         };
-
+        // bar chart data
         let bar_data = [trace1];
-
+        // bar chart layout/title
         let bar_layout = {
             title: `Top 10 OTUs for Subject ID ${result.id}`
         };
@@ -85,14 +86,19 @@ function updatePlots(sample) {
             x: otu_ids,
             y: sample_values,
             mode: "markers",
+            // set marker size as sample_values, color as otu_ids and colorscale to Jet
             marker: {size: sample_values, color: otu_ids, colorscale: "Jet"},
+            // set hovertext
             text: otu_labels 
         };
-
+        // bubble chart data
         let bubble_data = [trace2];
-
+        // bubble chart layout/title and xaxis title
         let bubble_layout = {
-            title: `OTUs by Sample`
+            title: "OTUs by Sample",
+            xaxis: {
+                title: "OTU ID"
+            }
         };
 
         // build Plotly plot with data and layout, assigned to .bubble id in html
@@ -103,11 +109,11 @@ function updatePlots(sample) {
 
 }
 
-// function to update the plots when a new sample is selected
+// function to update the plots when a new sample is selected in the drop-down
 function optionChanged(newSample) {
     updateMetadata(newSample);
     updatePlots(newSample);
 }
 
-// initialize the dashboard with the default plot
+// initialize the dashboard with the default plots
 init();
